4fdf61dcce4b5a6ce8473883891dc5858caed341
Tara-s1 <114695118+Tara-s1@users.noreply.github.com>
2024-09-27 17:21:48 -0500

    Update 1dof_sim.py
    change cd for each ads

--- 1dof_sim.py
+++ 1dof_sim.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Constants
 A_vehicle = 0.137  # Reference area of the vehicle 
 A_ads = [0, 0.021, 0.0278, 0.035, 0.0417, 0.167] # ADS areas
-Cd_ads = 1.0  # Drag coefficient of ADS (flat plate)
+Cd_ads = [1.0, 0.55, 0.55, 0.55, 0.7, 0.97]  # Drag coefficient of ADS (flat plate)
 
 # Initial conditions
 h0 = 1982.47  #Initial height at burnout (ft)
@@ -17,7 +17,7 @@ def drag_force_ADS(rho, v, A, Cd):
     return 0.5 * rho * v**2 * Cd * A
 
 
-def compute_apogee(time_in, height_in, velocity_in, acceleration_in, mass, A_vehicle, A_ads, dt=0.01):
+def compute_apogee(time_in, height_in, velocity_in, acceleration_in, mass, A_vehicle, A_ads, Cd_ads, dt=0.01):
     # Constants
     g = 32.174  # acceleration due to gravity in ft/s^2
     rho_air = 0.002378  # density of air in slugs/ft^3
@@ -35,7 +35,7 @@ def compute_apogee(time_in, height_in, velocity_in, acceleration_in, mass, A_veh
     
     while current_velocity > 0:
         times.append(current_time)
-        F_drag = 0.5 * rho_air * current_velocity**2 * Cd_vehicle * A_vehicle + drag_force_ADS(rho_air, current_velocity, A_ads, 1)
+        F_drag = 0.5 * rho_air * current_velocity**2 * Cd_vehicle * A_vehicle + drag_force_ADS(rho_air, current_velocity, A_ads, Cd_ads)
         F_gravity = mass * g
         F_net = -1 * F_drag - F_gravity
         acceleration_net = F_net / mass
@@ -52,9 +52,9 @@ def compute_apogee(time_in, height_in, velocity_in, acceleration_in, mass, A_veh
 # Simulate for different ADS areas and plot
 plt.figure(figsize=(10, 8))
 
-for a in A_ads:
-    times, heights, velocities = compute_apogee(0, h0, v0, a0, mass, A_vehicle, a)
-    plt.plot(times, heights, label=f'ADS Area = {a} ft²')
+for n in range(0,len(A_ads)):
+    times, heights, velocities = compute_apogee(0, h0, v0, a0, mass, A_vehicle, A_ads[n], Cd_ads[n])
+    plt.plot(times, heights, label=f'ADS Area = {A_ads[n]} ft²')
     #plt.scatter(times[-1],heights[-1], label=f'{heights[-1]}')
     plt.text(times[-1], heights[-1], f' {heights[-1]:.1f} ft', fontsize=5, ha='left', va='center')
 
