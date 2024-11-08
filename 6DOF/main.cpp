9817ed2f0127874da59c16fcf48371eeba8019f4
Tara-s1 <114695118+Tara-s1@users.noreply.github.com>
2024-11-08 09:53:37 -0600

    testing

--- 6DOF/main.cpp
+++ 6DOF/main.cpp
@@ -59,11 +59,13 @@ int main() {
             7, 8;
 
     // Print the matrices
-    std::cout << "Matrix A:\n" << matA << "\n\n";
+    std::cout << "Matrix A1:\n" << matA << "\n\n";
     std::cout << "Matrix B:\n" << matB << "\n\n";
 
     // Matrix addition
     Eigen::Matrix2d matSum = matA + matB;
     std::cout << "A + B:\n" << matSum << "\n\n";
+
+
 
 };
