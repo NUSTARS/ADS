7a61ebbdb0201856546f4c2fe0afc41d61b90eef
Tara-s1 <114695118+Tara-s1@users.noreply.github.com>
2024-11-08 18:14:50 -0600

    main.cpp for testing done

--- 6DOF/main.cpp
+++ 6DOF/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <Eigen/Core>
 #include "q.h"
-// #include "calcDynamics.h"
-// #include "constants.h"
+#include "calcDynamics.h"
+#include "constants.h"
+#include "simulator.h"
 #include <random>
 
+
+Eigen::Vector3d addGaussianNoise(Eigen::Vector3d value, double mean, double stddev) {
+    static std::default_random_engine generator;
+    std::normal_distribution<double> distribution(mean, stddev);
+
+    Eigen::Vector3d noiseVector;
+    noiseVector[0] = value[0] + distribution(generator);
+    noiseVector[1] = value[1] + distribution(generator);
+    noiseVector[2] = value[2] + distribution(generator);
+
+    return noiseVector;
+}
 
 double addGaussianNoise(double value, double mean, double stddev) {
     static std::default_random_engine generator;
@@ -12,61 +25,69 @@ double addGaussianNoise(double value, double mean, double stddev) {
     return value + distribution(generator);
 }
 
-q addSensorNoise(const q& currentState) {
-    double noise_mean = 0.0;
-    double noise_stddev = 0.01; // Standard deviation of noise, adjust as needed
 
-   /*return q(
-        addGaussianNoise(currentState.getVX(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getVY(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getVZ(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getOmegaX(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getOmegaY(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getOmegaZ(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getTheta(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getPsi(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getPhi(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getH(), noise_mean, noise_stddev),
-        addGaussianNoise(currentState.getU(), noise_mean, noise_stddev)
-    );*/
-    return q(Eigen::Vector3d(0,0,0),Eigen::Vector3d(0,0,0),Eigen::Vector3d(0,0,0), 0, 0);
+q addGaussianNoise(const q& currentState, double IMUmean, double IMUstddev, double BMPmean, double BMPstddev) {
+    return q(addGaussianNoise(currentState.getV(), IMUmean, IMUstddev),  
+                        addGaussianNoise(currentState.getOmega(), IMUmean, IMUstddev),
+                        addGaussianNoise(currentState.getTheta(), IMUmean, IMUstddev), 
+                        addGaussianNoise(currentState.getH(), BMPmean, BMPstddev),
+                        addGaussianNoise(currentState.getU(), BMPmean, BMPstddev));
+}
+
+Eigen::Vector3d calcVelocity(const q& currentState, double IMUmean, double IMUstddev, double BMPmean, double BMPstddev) {
+    //cannot apply error directly to velocity because never measure it 
+    q k1 = addGaussianNoise(getqdot(curr_q), IMUmean, IMUstddev,BMPmean, BMPstddev) * DT;
+    q k2 = addGaussianNoise(getqdot(curr_q + k1/2.0), IMUmean, IMUstddev,BMPmean, BMPstddev) * DT;
+    q k3 = addGaussianNoise(getqdot(curr_q + k2/2.0), IMUmean, IMUstddev,BMPmean, BMPstddev) * DT;
+    q k4 = addGaussianNoise(getqdot(curr_q + k3), IMUmean, IMUstddev,BMPmean, BMPstddev) * DT;
+    q new_q = curr_q + (k1 + k2*2.0 + k3*2.0 + k4) * (1/6.0);
+    Eigen::Vector3d new_velocity = new_q.getV();
+    return new_velocity; 
+
+}
+
+
+
+q addSensorNoise(const q& currentState) {
+
+    double IMU_noise_mean = 0.0;
+    double IMU_noise_stddev = 0.01;
+    double BMP_noise_mean = 0.0;
+    double BMP_noise_stddev = 0.01;
+
+    //add noise to the IMU and barometer (velocity)
+
+    Eigen::Vector3d new_vel = calcVelocity(currentState, IMU_noise_mean, IMU_noise_stddev, BMP_noise_mean, BMP_noise_stddev);
+    
+    return q(new_vel, 
+            addGaussianNoise(currentState.getOmega(),IMU_noise_mean, IMU_noise_stddev), 
+            addGaussianNoise(currentState.getTheta(), IMU_noise_mean, IMU_noise_stddev), 
+            addGaussianNoise(currentState.getH(), BMP_noise_mean, BMP_noise_stddev), 
+            currentState.getU());
 }
 
 int main() {
     // post boost initial state -- fill in with real numbers
     q currentState(Eigen::Vector3d(0,0,0), Eigen::Vector3d(0,0,0), Eigen::Vector3d(0,0,0), 0, 0);
-    double dt = 0.1;
+
     double max_simTime = 30;
     double current_t = 0.0;
 
-    //run while the sim is less than 30 seconds and havent reached apogee 
-    // while ((current_t < max_simTime) && !(at_apogee(currentState))) {
 
-    //     q sensorReadingState = addSensorNoise(currentState);
-    //     currentState = simulate(sensorReadingState, dt);
+    // run while the sim is less than 30 seconds and havent reached apogee 
+    while ((current_t < max_simTime) && !(atApogee(currentState))) {
 
-    // }
+        q SensorNoiseState = addSensorNoise(currentState);
+
+        //call SHISHIR CODE to get b, set B in current state going forward
 
 
 
-    // Define a 2x2 matrix with predefined values
-    Eigen::Matrix2d matA;
-    matA << 1, 2,
-            3, 4;
+        currentState = integrate(SensorNoiseState);
 
-    // Define another 2x2 matrix
-    Eigen::Matrix2d matB;
-    matB << 5, 6, 
-            7, 8;
+        current_t += DT;
 
-    // Print the matrices
-    std::cout << "Matrix A1:\n" << matA << "\n\n";
-    std::cout << "Matrix B:\n" << matB << "\n\n";
-
-    // Matrix addition
-    Eigen::Matrix2d matSum = matA + matB;
-    std::cout << "A + B:\n" << matSum << "\n\n";
-
+    }
 
 
 };
