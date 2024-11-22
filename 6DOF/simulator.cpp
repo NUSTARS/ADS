3d87119d6c01d030289b4f2a20400458088acce2
shishirb12 <66386787+shishirb12@users.noreply.github.com>
2024-11-22 17:09:57 -0600

    Update simulator.cpp

--- 6DOF/simulator.cpp
+++ 6DOF/simulator.cpp
@@ -115,6 +115,8 @@ double getApogee(q curr_q, double b){
     
     std::ofstream outfile("data.csv");
 	
+	outfile << 'time' << "," << 'alts' << "," << 'vx' << "," << 'vy' << "," << 'vz' << "," << 'wx' << "," << 'wy' << "," << 'wz' << "," << 'thetax' << "," << 'thetay' << "," << 'thetaz' << "\n";
+	
 	// Write data rows
     for (size_t i = 0; i < alts.size(); ++i) {
         outfile << times[i] << "," << alts[i] << "," << velocx[i] << "," << velocy[i] << "," << velocz[i] << "," << omegax[i] << "," << omegay[i] << "," << omegaz[i] << "," << thetax[i] << "," << thetay[i] << "," << thetaz[i] << "\n";
