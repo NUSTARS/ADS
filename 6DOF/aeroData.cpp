44287131b24a81162e198952ebcc7d8d21ef2dec
max-hug <maxhug@u.northwestern.edu>
2025-01-27 20:04:38 -0600

    updated with polynomial fits

--- 6DOF/aeroData.cpp
+++ 6DOF/aeroData.cpp
@@ -18,7 +18,12 @@
 // Takes in vSquared, angle of attack, control signal, and altitude to output the current Cd
 //
 double getCD(double vSquared, double alpha, double u, double h){
-    return (0.481+u/2.0) + (vSquared*1.15E-07); // TODO get this from CFD ppl
+    //return (0.481+u/2.0) + (vSquared*1.15E-07); // TODO get this from CFD ppl
+
+    double alphaDeg = alpha * 3.1415/180.0;
+    double v = sqrt(vSquared);
+    
+    return -0.0021716*alphaDeg - 6.4358e-06*v + 1.8701e-06*v*alpha + 1.2254e-07*vSquared + 0.0020791*pow(alpha,2) + 0.48279;
 }
 
 //
@@ -27,7 +32,13 @@ double getCD(double vSquared, double alpha, double u, double h){
 // Takes in vSquared, angle of attack, control signal, and altitude to output the current Cn
 //
 double getCN(double vSquared, double alpha, double u, double h){
-    return pow(alpha,1.25)*(25+u/2.0); // TODO get this from CFD ppl
+
+    double alphaDeg = alpha * 3.1415/180.0;
+    double v = sqrt(vSquared);
+
+    return 0.18966*alphaDeg - 2.7885e-06*v + 1.7577e-05*alphaDeg*v + 0.00051704 + 3.4281e-09*vSquared + 0.010233*pow(alphaDeg,2);
+
+    //return pow(alpha,1.25)*(25+u/2.0); // TODO get this from CFD ppl
 }
 
 
@@ -39,10 +50,10 @@ double getCN(double vSquared, double alpha, double u, double h){
 //
 double getCP(double vSquared, double alpha, double u, double h){
 
-    double x = sqrt(vSquared); // x is the square root of vsquared
+    double alphaDeg = alpha * 3.1415/180.0;
+    double v = sqrt(vSquared);
 
-    return 63.5 + 0.526 * x + (-5.28E-03) * pow(x, 2) + 2.95E-05 * pow(x, 3) + (-9.95E-08) * pow(x, 4) + 
-           2.08E-10 * pow(x, 5) + (-2.63E-13) * pow(x, 6) + 1.84E-16 * pow(x, 7) + (-5.49E-20) * pow(x, 8);
+    return -1.2251*alphaDeg - 0.000270051*v + 0.00015956*alphaDeg*v + 85.9529 + 2.10647e-06*vSquared + 0.0692183*pow(alphaDeg,2);
 
     // return (pow(vSquared,1.27/2.0)*0.00025 + 85.7)/12.0; // TEMP
 }
