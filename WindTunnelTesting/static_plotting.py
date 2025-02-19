949cffb4fc6a13c7cf99b4a099678b75ba42be41
thereal-adubs <112673487+thereal-adubs@users.noreply.github.com>
2025-02-19 14:29:22 -0600

    created generic plotting function for wind tunnel analysis

--- WindTunnelTesting/static_plotting.py
+++ WindTunnelTesting/static_plotting.py
@@ -2,38 +2,38 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+'''
+Plotting function to plot data based on the filtered dataframe
+'''
+def plot_data(filtered_df, x_col, y_col, group_by, scatter, title, x_axis, y_axis):
+    groups = filtered_df.groupby(group_by)
+    plt.figure(figsize=(10, 6))
+    for group_by, group in groups:
+        sorted_group = group.sort_values(by=x_col)
+        if scatter:
+            plt.scatter(sorted_group[x_col], sorted_group[y_col], label=f"{group_by}", marker='o')
+        else:
+            plt.plot(sorted_group[x_col], sorted_group[y_col], label=f"{group_by}", marker='o')
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.title(title)
+    plt.grid(True)
+    plt.legend(loc='best', fontsize='small', frameon=True)
 
 project_root = Path(__file__).parent 
-
 file_path = project_root / "static_clean.csv"
 df = pd.read_csv(file_path)
 
 filtered_df = df[(df["Yaw"] == 0) & (df["Actuation State"].isin([0, 20, 50, 70, 100]))]
+plot_data(filtered_df, "Reynolds Number", "ADS Drag", "Actuation State", False, "Drag Force vs. Reynolds Number for Yaw=0 and Selected Actuation States", "Reynolds Number", "Drag Force (ADS)")
 
-# Group data by Actuation State
-groups = filtered_df.groupby("Actuation State")
+filtered_df = df[(df["Yaw"] == 0) & (df["Actuation State"].isin([0, 20, 50, 70, 100]))]
+plot_data(filtered_df, "Reynolds Number", "Cd", "Actuation State", False, "Cd vs. Reynolds Number for Yaw=0 and Selected Actuation States", "Reynolds Number", "Cd")
 
-# Plot
-plt.figure(figsize=(10, 6))
-for actuation, group in groups:
-    sorted_group = group.sort_values(by="Reynolds Number per ft")
-    plt.plot(sorted_group["Reynolds Number"], sorted_group["ADS Drag"], label=f"Actuation {actuation}", marker='o')
+filtered_df = df
+plot_data(filtered_df, "Reynolds Number", "WAFBC Yaw", "Yaw", True, "Yaw vs Re for Selected Yaw Angles", "Re", "Yaw Moment [lbf-ft]")
 
-plt.xlabel("Reynolds Number")
-plt.ylabel("Drag Force (ADS)")
-plt.title("Drag Force vs. Reynolds Number for Yaw=0 and Selected Actuation States")
-plt.legend(loc='best', fontsize='small', frameon=True)
-plt.grid(True)
+# filtered_df = df
+# plot_data(filtered_df, "Reynolds Number", "WAFBC Lift", "Yaw", True, "Lift vs Re for Selected Yaw Angles", "Re", "Lift Force [lbf]")
 
-plt.figure(figsize=(10, 6))
-for actuation, group in groups:
-    sorted_group = group.sort_values(by="Reynolds Number per ft")
-    plt.plot(sorted_group["Reynolds Number"], sorted_group["Cd"], label=f"Actuation {actuation}", marker='o')
-
-plt.xlabel("Reynolds Number")
-plt.ylabel("Cd")
-plt.title("Cd vs. Reynolds Number for Yaw=0 and Selected Actuation States")
-plt.legend(loc='best', fontsize='small', frameon=True)
-plt.grid(True)
 plt.show()
-
